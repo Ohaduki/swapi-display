972c0a90b69ebb34af3901401c48dac61ba78550
Ohad <ohad.licht@gmail.com>
2023-11-08 03:41:52 +0200

    fixed bug where clicking movie doesn't select it, and connected favotite toggle function to moviedetails component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieDetails movie={selectedMovie} favorites={favorites} />
+      <MovieDetails movie={selectedMovie} favorites={favorites} onFavoriteToggle={handleFavorite}/>
       <MovieList onMovieSelect={handleMovieSelect}/>
     </div>
   );
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MovieItem({ movie, onMovieSelect }) {
   return (
     <div key={movie.episode_id}>
-      <button onClick={() => onMovieSelect()}>
+      <button onClick={() => onMovieSelect(movie)}>
         View {movie.title}
       </button>
     </div>
